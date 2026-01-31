3dbda9d192c2558a2674bcb60cd9e3e6ccea8e21
a3emond <a3emond@gmail.com>
2026-01-31 11:10:28 -0500

    Devoir 1

--- mon-app-angular/src/app/components/list-products/list-products.component.ts
+++ mon-app-angular/src/app/components/list-products/list-products.component.ts
@@ -1,24 +1,61 @@
 import { Component } from '@angular/core';
-import { ProductItemComponent } from '../product-item/product-item.component';
 import { CommonModule } from '@angular/common';
+import { ProductItemComponent } from '../product-item/product-item.component';
+
+type CartItem = {
+  product: any;
+  quantity: number;
+};
 
 @Component({
   selector: 'app-list-products',
-  imports: [ProductItemComponent, CommonModule],
+  standalone: true,
+  imports: [CommonModule, ProductItemComponent],
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.scss'
 })
 export class ListProductsComponent {
   products = [
-    {id: 1, name: "Product A", price: 10.99},
-    {id: 2, name: "Product B", price: 7.99},
-    {id: 3, name: "Product C", price: 3.99},
-  ]
-  sum = 0;
+    { id: 1, name: 'Product A', price: 10.99 },
+    { id: 2, name: 'Product B', price: 7.99 },
+    { id: 3, name: 'Product C', price: 3.99 }
+  ];
 
-  onAddToCart(product: any){
-    console.log("PARENT LIST PRODUCTS!");
-    console.log(product);
-    this.sum += product.price;
+  cart: CartItem[] = [];
+
+  onAddToCart(product: any) {
+    const item = this.cart.find(i => i.product.id === product.id);
+
+    if (item) {
+      item.quantity++;
+    } else {
+      this.cart.push({ product, quantity: 1 });
+    }
+  }
+
+  increaseQuantity(productId: number) {
+    const item = this.cart.find(i => i.product.id === productId);
+    if (item) item.quantity++;
+  }
+
+  decreaseQuantity(productId: number) {
+    const item = this.cart.find(i => i.product.id === productId);
+    if (!item) return;
+
+    item.quantity--;
+    if (item.quantity <= 0) {
+      this.onRemoveFromCart(productId);
+    }
+  }
+
+  onRemoveFromCart(productId: number) {
+    this.cart = this.cart.filter(i => i.product.id !== productId);
+  }
+
+  get total() {
+    return this.cart.reduce(
+      (sum, i) => sum + i.product.price * i.quantity,
+      0
+    );
   }
 }
--- mon-app-angular/src/app/components/product-item/product-item.component.ts
+++ mon-app-angular/src/app/components/product-item/product-item.component.ts
@@ -2,16 +2,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
+  standalone: true,
   imports: [],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
 })
 export class ProductItemComponent {
-  @Input() product: any;
-  @Output() addToCart = new EventEmitter();
+  @Input() product!: any;
+  @Output() addToCart = new EventEmitter<any>();
 
-  onAddToCart(){
-    console.log(this.product);
+  onAddToCart() {
     this.addToCart.emit(this.product);
   }
 }
