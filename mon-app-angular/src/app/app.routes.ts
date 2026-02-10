c1eb138f5379c73a6130aaf979d399f4e480c854
Renan <renan_cavalcanti@hotmail.com>
2026-02-10 09:54:52 -0500

    feat: service + api integration

--- mon-app-angular/src/app/app.routes.ts
+++ mon-app-angular/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CounterComponent } from './components/counter/counter.component';
 import { ListProductsComponent } from './components/list-products/list-products.component';
+import { GithubUsersComponent } from './components/github-users/github-users.component';
 
 export const routes: Routes = [
   {path: 'counter', component: CounterComponent},
-  {path: 'products', component: ListProductsComponent}
+  {path: 'products', component: ListProductsComponent},
+  {path: 'github', component: GithubUsersComponent}
 ];
