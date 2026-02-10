254ae6ca6af02fb3b32a1c773fc4319448c5f32a
Renan <renan_cavalcanti@hotmail.com>
2026-02-10 11:51:45 -0500

    feat: api materials integration

--- mon-app-angular/src/app/services/github.service.ts
+++ mon-app-angular/src/app/services/github.service.ts
@@ -9,7 +9,7 @@ export class GithubService {
 
   private readonly API_URL = "https://api.github.com/users";
 
-  constructor(private http: HttpClient) { }
+  constructor(private readonly http: HttpClient) { }
 
   getUser(username: string): Observable<any>{
     return this.http.get<any>(`${this.API_URL}/${username}`);
